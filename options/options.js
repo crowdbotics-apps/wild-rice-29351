6f6dafebe92cfdbb477bd057c33285ffc962729c
Crowdbotics <team@crowdbotics.com>
2021-08-02 22:46:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wild_rice_29351Identifier",
+  url: "https://wild_rice_29351Identifier.botics.co",
+  api: "https://wild_rice_29351Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
